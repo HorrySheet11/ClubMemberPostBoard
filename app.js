80dd9b7144790c300696869e522d046ff8402d12
HorrySheet11 <marinnathan11@gmail.com>
2026-01-26 19:29:58 +0800

    implemented working passport login authentication

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ const path = require("node:path");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const routes = require("./routers/usersRouter");
-const passwordController = require("./controllers/passportController");
-
+require("./config/passport");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(
+  session({ secret: "secretttt", resave: false, saveUninitialized: false }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
+	console.log(req.session);
+	console.log(req.user);
 	next();
 });
 app.use("/", routes);
-app.use(
-	session({ secret: "secretttt", resave: false, saveUninitialized: false }),
-);
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000, (error) => {
+app.listen(4000, (error) => {
 	if (error) {
 		throw error;
 	}
---
+++ config/passport.js
@@ -0,0 +1,55 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const pool = require("../db/pool");
+
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: "email",
+			passwordField: "password",
+			passReqToCallback: true,
+		},
+		async (req, username, password, done) => {
+			const firstname = req.body.firstname;
+			const lastname = req.body.lastname;
+			try {
+				const { rows } = await pool.query(
+					"SELECT * FROM members WHERE firstname = $1 and lastname = $2 and email = $3",
+					[firstname, lastname, username],
+				);
+				const user = rows[0];
+				if (!user) {
+					return done(null, false, { message: "Incorrect first or last name" });
+				}
+				const match = await bcrypt.compare(password, user.password);
+				if (!match) {
+					return done(null, false, { message: "Incorrect password" });
+				}
+				return done(null, user);
+			} catch (err) {
+				console.error(err);
+				return done(err);
+			}
+		},
+	),
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.member_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const { rows } = await pool.query(
+			"SELECT * FROM members WHERE member_id = $1",
+			[id],
+		);
+		const user = rows[0];
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
+
+module.exports = passport;
