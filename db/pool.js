56f219693cd8de894f40f11d6163f0a81d2e7dbd
HorrySheet11 <marinnathan11@gmail.com>
2026-01-29 21:08:11 +0800

    edited db.js with connectionString property

--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,13 @@ const { Pool } = require("pg");
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
